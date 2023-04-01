db5165adbca69e38cd0d471fe1ef9ec8ca6da7a2
riccardodebenedictis <riccardo.debenedictis@istc.cnr.it>
2023-04-01 12:55:39 +0200

    added initial client..

---
+++ client/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+    <v-navigation-drawer v-model="drawer">
+      <v-list dense>
+        <SensorType v-for="sensor_type in sensor_types" :key="sensor_type.id" :sensor_type="sensor_type" />
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>CoCo</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({ drawer: false }),
+}
+</script>
+
+<script setup>
+import { useAppStore } from './store/app.js'
+import { storeToRefs } from 'pinia';
+
+import SensorType from './components/SensorType.vue'
+
+const { sensor_types } = storeToRefs(useAppStore())
+</script>
---
+++ client/src/components/SensorType.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-list-item>
+    <v-list-item-content>
+      <v-list-item-title>{{ sensor_type.name }}</v-list-item-title>
+    </v-list-item-content>
+  </v-list-item>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+defineProps({
+  sensor_type: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
