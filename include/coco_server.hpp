8de15d6c045e0777a8c00e79fdd6b6ac4a8e0002
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2024-04-25 19:37:21 +0200

    Update submodule commits for extern/coco and extern/rationet

--- include/coco_server.hpp
+++ include/coco_server.hpp
@@ -1,1 +1,13 @@
 #pragma once
+
+#include "coco_core.hpp"
+#include "server.hpp"
+
+namespace coco
+{
+    class coco_server : public network::server
+    {
+    public:
+        coco_server();
+    };
+} // namespace coco
