8a983ddf8b2a22ebc63a758a36608ebb37349767
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2024-04-26 09:52:12 +0200

    Added initial client implementation..

---
+++ client/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <v-navigation-drawer v-model="drawer">
+    </v-navigation-drawer>
+
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-title>CoCo</v-app-bar-title>
+    </v-app-bar>
+
+    <v-main>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const drawer = ref(false)
+</script>
