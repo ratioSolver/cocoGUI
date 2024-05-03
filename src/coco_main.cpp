8544eb43591930bb9f737abef35fb923df0b2934
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2024-05-03 17:09:55 +0200

    Update mongocxx include in coco_main.cpp

--- src/coco_main.cpp
+++ src/coco_main.cpp
@@ -1,7 +1,10 @@
+#include <mongocxx/instance.hpp>
 #include "coco_server.hpp"
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char const *argv[])
 {
+    mongocxx::instance inst{}; // This should be done only once.
+
     coco::coco_server server;
     server.start();
     return 0;
