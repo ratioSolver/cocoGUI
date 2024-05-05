6608375f69d99e3443a651753039e2fd7414179a
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2024-05-05 12:26:26 +0200

    Update coco_server to include open_api and async_api routes

--- include/coco_server.hpp
+++ include/coco_server.hpp
@@ -16,6 +16,8 @@ namespace coco
     private:
         std::unique_ptr<network::response> index(network::request &req);
         std::unique_ptr<network::response> assets(network::request &req);
+        std::unique_ptr<network::response> open_api(network::request &req);
+        std::unique_ptr<network::response> async_api(network::request &req);
 
         void on_ws_open(network::ws_session &ws);
         void on_ws_message(network::ws_session &ws, const std::string &msg);
@@ -23,6 +25,10 @@ namespace coco
         void on_ws_error(network::ws_session &ws);
 
     private:
+        json::json j_open_api{{"openapi", "3.1.0"},
+                              {"info", {{"title", "Coco API"}, {"version", "1.0"}}}};
+        json::json j_async_api{{"asyncapi", "3.0.0"},
+                               {"info", {{"title", "Coco API"}, {"version", "1.0"}}}};
         std::unordered_set<network::ws_session *> clients;
     };
 } // namespace coco
--- src/coco_server.cpp
+++ src/coco_server.cpp
@@ -6,8 +6,23 @@ namespace coco
 {
     coco_server::coco_server() : coco_core(std::make_unique<mongo_db>()), network::server()
     {
+        json::json open_api_servers(json::json_type::array);
+        open_api_servers.push_back({{"url", "http://" SERVER_HOST ":" + std::to_string(SERVER_PORT)}});
+        j_open_api["servers"] = std::move(open_api_servers);
+        json::json open_api_paths(json::json_type::array);
+        json::json open_api_index_path{{"url", "/"}, {"method", "GET"}, {"summary", "Index"}, {"description", "Index page"}};
+        open_api_paths.push_back(std::move(open_api_index_path));
+        json::json open_api_assets_path{{"url", "/assets/{file}"}, {"method", "GET"}, {"summary", "Assets"}, {"description", "Assets"}};
+        open_api_paths.push_back(std::move(open_api_assets_path));
+        json::json open_api_api_path{{"url", "/api"}, {"method", "GET"}, {"summary", "API"}, {"description", "API"}};
+        open_api_paths.push_back(std::move(open_api_api_path));
+        j_open_api["paths"] = std::move(open_api_paths);
+        LOG_INFO(j_open_api);
+
         add_route(network::GET, "^/$", std::bind(&coco_server::index, this, std::placeholders::_1));
         add_route(network::GET, "^(/assets/.+)|/.+\\.ico|/.+\\.png", std::bind(&coco_server::assets, this, std::placeholders::_1));
+        add_route(network::GET, "^/open_api$", std::bind(&coco_server::open_api, this, std::placeholders::_1));
+        add_route(network::GET, "^/async_api$", std::bind(&coco_server::async_api, this, std::placeholders::_1));
 
         add_ws_route("/coco").on_open(std::bind(&coco_server::on_ws_open, this, std::placeholders::_1)).on_message(std::bind(&coco_server::on_ws_message, this, std::placeholders::_1, std::placeholders::_2)).on_close(std::bind(&coco_server::on_ws_close, this, std::placeholders::_1)).on_error(std::bind(&coco_server::on_ws_error, this, std::placeholders::_1));
     }
@@ -20,6 +35,8 @@ namespace coco
             target = target.substr(0, target.find('?'));
         return std::make_unique<network::file_response>("client/dist" + target);
     }
+    std::unique_ptr<network::response> coco_server::open_api(network::request &) { return std::make_unique<network::json_response>(j_open_api); }
+    std::unique_ptr<network::response> coco_server::async_api(network::request &) { return std::make_unique<network::json_response>(j_async_api); }
 
     void coco_server::on_ws_open(network::ws_session &ws) { clients.insert(&ws); }
     void coco_server::on_ws_message(network::ws_session &ws, const std::string &msg)
