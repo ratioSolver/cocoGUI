617706c63099a1ef01644875d6e466dbb1b8d175
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2024-06-10 19:19:21 +0200

    Improved item.js

--- client/src/item.js
+++ client/src/item.js
@@ -1,15 +1,49 @@
+/**
+ * Represents a parameter.
+ */
 export class Parameter {
-
-    constructor(name, default_value = undefined) { this.name = name; this.default_value = default_value; }
+    /**
+     * Creates a new Parameter instance.
+     * 
+     * @param {string} name - The name of the parameter.
+     * @param {*} [default_value] - The default value of the parameter (optional).
+     */
+    constructor(name, default_value = undefined) {
+        this.name = name;
+        this.default_value = default_value;
+    }
 }
 
+/**
+ * Represents a boolean parameter.
+ * @extends Parameter
+ */
 export class BooleanParameter extends Parameter {
-
-    constructor(name, default_value = false) { super(name, default_value); }
+    /**
+     * Creates a new BooleanParameter instance.
+     * 
+     * @param {string} name - The name of the parameter.
+     * @param {boolean} [default_value=false] - The default value of the parameter.
+     */
+    constructor(name, default_value = false) {
+        super(name, default_value);
+    }
 }
 
+/**
+ * Represents an integer parameter.
+ * @extends Parameter
+ */
 export class IntegerParameter extends Parameter {
 
+    /**
+     * Creates a new IntegerParameter instance.
+     * 
+     * @param {string} name - The name of the parameter.
+     * @param {number} min - The minimum value of the parameter.
+     * @param {number} max - The maximum value of the parameter.
+     * @param {number} [default_value=min] - The default value of the parameter.
+     */
     constructor(name, min, max, default_value = min) {
         super(name, default_value);
         this.min = min;
@@ -17,8 +51,20 @@ export class IntegerParameter extends Parameter {
     }
 }
 
+/**
+ * Represents a real parameter.
+ * @extends Parameter
+ */
 export class RealParameter extends Parameter {
 
+    /**
+     * Creates a new instance of the RealParameter class.
+     * 
+     * @param {string} name - The name of the parameter.
+     * @param {number} min - The minimum value of the parameter.
+     * @param {number} max - The maximum value of the parameter.
+     * @param {number} [default_value=min] - The default value of the parameter.
+     */
     constructor(name, min, max, default_value = min) {
         super(name, default_value);
         this.min = min;
@@ -26,21 +72,83 @@ export class RealParameter extends Parameter {
     }
 }
 
+/**
+ * Represents a string parameter.
+ * @extends Parameter
+ */
 export class StringParameter extends Parameter {
-
-    constructor(name, default_value = "") { super(name, default_value); }
+    /**
+     * Creates a new instance of the StringParameter class.
+     * 
+     * @param {string} name - The name of the parameter.
+     * @param {string} [default_value=""] - The default value of the parameter.
+     */
+    constructor(name, default_value = "") {
+        super(name, default_value);
+    }
 }
 
+/**
+ * Represents a symbol parameter.
+ * @extends Parameter
+ */
 export class SymbolParameter extends Parameter {
 
+    /**
+     * Creates a new SymbolParameter instance.
+     * 
+     * @param {string} name - The name of the parameter.
+     * @param {Array} symbols - The symbols for the parameter.
+     * @param {string} [default_value=symbols[0]] - The default value for the parameter.
+     */
     constructor(name, symbols, default_value = symbols ? symbols[0] : "") {
         super(name, default_value);
         this.symbols = symbols;
     }
 }
 
+function create_default_array(shape, array_type) {
+    if (shape.length === 0) return array_type.default_value;
+    let array = new Array(shape[0]);
+    for (let i = 0; i < shape[0]; i++) array[i] = create_default_array(shape.slice(1), array_type);
+    return array;
+}
+
+/**
+ * Represents an array parameter.
+ * @extends Parameter
+ */
+export class ArrayParameter extends Parameter {
+
+    /**
+     * Creates a new ArrayParameter instance.
+     * 
+     * @param {string} name - The name of the parameter.
+     * @param {string} array_type - The type of the elements in the array.
+     * @param {Array<number>} shape - The shape of the array.
+     * @param {Array<any>} [default_value=create_default_array(shape, array_type)] - The default value of the parameter.
+     */
+    constructor(name, array_type, shape, default_value = create_default_array(shape, array_type)) {
+        super(name, default_value);
+        this.array_type = array_type;
+        this.shape = shape;
+    }
+}
+
+/**
+ * Represents a type of item.
+ */
 export class Type {
 
+    /**
+     * Creates a new instance of the Type class.
+     * 
+     * @param {number} id - The ID of the type.
+     * @param {string} name - The name of the type.
+     * @param {string} description - The description of the type.
+     * @param {Array} static_parameters - The static parameters of the type.
+     * @param {Array} dynamic_parameters - The dynamic parameters of the type.
+     */
     constructor(id, name, description, static_parameters, dynamic_parameters) {
         this.id = id;
         this.name = name;
@@ -50,8 +158,20 @@ export class Type {
     }
 }
 
+/**
+ * Represents an item.
+ */
 export class Item {
 
+    /**
+     * Creates a new instance of the Item class.
+     * 
+     * @param {string} id - The ID of the item.
+     * @param {string} name - The name of the item.
+     * @param {string} type - The type of the item.
+     * @param {string} [description=""] - The description of the item (optional).
+     * @param {Object} [parameters={}] - The parameters of the item (optional).
+     */
     constructor(id, name, type, description = "", parameters = {}) {
         this.id = id;
         this.name = name;
@@ -66,6 +186,12 @@ export class Item {
         this.value_listeners = new Set();
     }
 
+    /**
+     * Sets the values and timestamps of the item.
+     * 
+     * @param {Array} values - The values of the item.
+     * @param {Array} timestamps - The timestamps of the values.
+     */
     set_values(values, timestamps) {
         this.values = values;
         this.valueTimestamps = timestamps;
@@ -74,6 +200,12 @@ export class Item {
         this.values_listeners.forEach(l => l(values, timestamps));
     }
 
+    /**
+     * Adds a value and timestamp to the item.
+     * 
+     * @param {*} value - The value to add.
+     * @param {number} [timestamp=new Date().getTime()] - The timestamp of the value (optional).
+     */
     add_value(value, timestamp = new Date().getTime()) {
         this.lastValue = value;
         this.lastUpdate = timestamp;
@@ -82,9 +214,90 @@ export class Item {
         this.value_listeners.forEach(l => l(value, timestamp));
     }
 
-    add_values_listener(callback) { this.values_listeners.add(callback); callback(this.values, this.valueTimestamps); }
-    add_value_listener(callback) { this.value_listeners.add(callback); }
+    /**
+     * Adds a listener for values changes.
+     * 
+     * @param {Function} callback - The callback function to be called when values change.
+     */
+    add_values_listener(callback) {
+        this.values_listeners.add(callback);
+        callback(this.values, this.valueTimestamps);
+    }
 
-    remove_values_listener(callback) { this.values_listeners.delete(callback); }
-    remove_value_listener(callback) { this.value_listeners.delete(callback); }
+    /**
+     * Adds a listener for value changes.
+     * 
+     * @param {Function} callback - The callback function to be called when a value changes.
+     */
+    add_value_listener(callback) {
+        this.value_listeners.add(callback);
+    }
+
+    /**
+     * Removes a listener for values changes.
+     * 
+     * @param {Function} callback - The callback function to be removed.
+     */
+    remove_values_listener(callback) {
+        this.values_listeners.delete(callback);
+    }
+
+    /**
+     * Removes a listener for value changes.
+     * 
+     * @param {Function} callback - The callback function to be removed.
+     */
+    remove_value_listener(callback) {
+        this.value_listeners.delete(callback);
+    }
+}
+
+function get_parameter_type(par) {
+    switch (par.type) {
+        case 'int':
+            return new IntegerParameter(par.name, par.min === undefined ? -Infinity : par.min, par.max === undefined ? Infinity : par.max);
+        case 'real':
+            return new RealParameter(par.name, par.min === undefined ? -Infinity : par.min, par.max === undefined ? Infinity : par.max);
+        case 'bool':
+            return new BooleanParameter(par.name);
+        case 'symbol':
+            return new SymbolParameter(par.name, par.symbols);
+        case 'string':
+            return new StringParameter(par.name);
+        case 'array':
+            return new ArrayParameter(par.name, get_parameter_type(par.array_type), par.shape);
+        default:
+            throw 'Unknown parameter type: ' + par;
+    }
+}
+
+function get_parameters(pars) {
+    let par_map = new Map();
+    pars.forEach(par => par_map.set(par.name, get_parameter_type(par)));
+    return par_map;
+}
+
+/**
+ * Retrieves a map of types based on the provided array of types.
+ *
+ * @param {Array} types - The array of types to be processed.
+ * @returns {Map} - A map of types, where the type ID is the key and the Type object is the value.
+ */
+export function get_types(types) {
+    let type_map = new Map();
+    types.forEach(type => type_map.set(type.id, new Type(type.id, type.name, type.description, get_parameters(type.static_parameters), get_parameters(type.dynamic_parameters))));
+    return type_map;
+}
+
+/**
+ * Creates a map of items based on the provided items array and types map.
+ *
+ * @param {Array} items - The array of items.
+ * @param {Map} types - The map of item types.
+ * @returns {Map} - A map of items with item IDs as keys and Item objects as values.
+ */
+export function get_items(items, types) {
+    let item_map = new Map();
+    items.forEach(item => item_map.set(item.id, new Item(item.id, item.name, types.get(item.type), item.description, item.parameters)));
+    return item_map;
 }
