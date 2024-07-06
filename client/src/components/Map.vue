ca16302d0f1e159f5ce13c0ffb8b8243da09b579
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2024-07-06 19:29:10 +0200

    chore: Add Leaflet dependency and initialize map component in Map.vue

---
+++ client/src/components/Map.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container id="map" class="fill-height" fluid />
+</template>
+
+<script setup lang="ts">
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+const props = defineProps<{ center: { lat: number; lng: number }; zoom: number; }>();
+
+let map = null;
+
+onMounted(() => {
+  map = L.map('map').setView([props.center.lat, props.center.lng], props.zoom);
+});
+</script>
