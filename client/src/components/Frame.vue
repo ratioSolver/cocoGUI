37f3ea3311a7c913fcbd6478bf3ec97620ba5beb
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2024-07-07 16:17:55 +0200

    refactor: Add Frame component for maximizing and minimizing content

---
+++ client/src/components/Frame.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-card :class="{ 'maximized': maximized.value }">
+    <v-toolbar>
+      <v-toolbar-title>{{ props.title }}</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon @click="toggle_maximized">
+        <v-icon>{{ maximized.value ? 'mdi-window-minimize' : 'mdi-window-maximize' }}</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-card-text>
+      <slot></slot>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const props = defineProps<{ title: string; }>();
+
+const maximized = ref(false);
+
+function toggle_maximized() {
+  maximized.value = !maximized.value;
+}
+</script>
+
+<style scoped>
+.maximized {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 1000;
+}
+</style>
