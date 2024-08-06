3aaf919bca274c012b57d88ed9be926e990e7a2d
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2024-08-06 09:13:57 +0200

    refactor: Add IntegerProperty, RealProperty, StringProperty, SymbolProperty, and ItemProperty components

--- coco-lib/src/index.ts
+++ coco-lib/src/index.ts
@@ -1,5 +1,10 @@
 import BooleanProperty from '@/components/properties/BooleanProperty.vue';
+import IntegerProperty from './components/properties/IntegerProperty.vue';
+import RealProperty from './components/properties/RealProperty.vue';
+import StringProperty from './components/properties/StringProperty.vue';
+import SymbolProperty from './components/properties/SymbolProperty.vue';
+import ItemProperty from './components/properties/ItemProperty.vue';
 
 export * from './coco';
 export * from './values';
-export { BooleanProperty };
+export { BooleanProperty, IntegerProperty, RealProperty, StringProperty, SymbolProperty, ItemProperty };
