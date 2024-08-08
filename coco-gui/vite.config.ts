9df4b8763547faa04e71ad09db1339eae08238ee
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2024-08-08 14:27:40 +0200

    refactor: Update coco-lib package dependencies and exports

--- coco-gui/vite.config.ts
+++ coco-gui/vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from 'node:url'
-import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [vue(), dts()],
+  plugins: [vue()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
