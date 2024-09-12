88b2369a0d43f5c030c0413b23f6256a437047a3
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2024-09-12 19:32:39 +0200

    Refactor key generation in CocoMap.vue and improve slider functionality

--- coco-gui/src/components/CocoMap.vue
+++ coco-gui/src/components/CocoMap.vue
@@ -10,7 +10,7 @@
 </template>
 
 <script setup lang="ts">
-import { NGrid, NGridItem, NSlider } from 'naive-ui';
+import { c, NGrid, NGridItem, NSlider } from 'naive-ui';
 import { h, onMounted, onUnmounted, ref, watch } from 'vue';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
@@ -27,7 +27,7 @@ const options: Map<taxonomy.Type, L.GeoJSONOptions> = new Map();
 const static_layers: Map<taxonomy.Type, L.Layer> = new Map();
 const dynamic_layers: Map<taxonomy.Type, L.Layer> = new Map();
 const dynamic_features: Map<number, Map<taxonomy.Type, GeoJSON.FeatureCollection>> = new Map();
-const dynamic_items: Map<taxonomy.Type, Map<taxonomy.Item, MapItemListener>> = new Map();
+const dynamic_items: Map<taxonomy.Type, { items: Map<taxonomy.Item, MapItemListener>, loaded: boolean }> = new Map();
 const show_slider = ref(false);
 const min = ref(Number.MAX_SAFE_INTEGER);
 const max = ref(Number.MIN_SAFE_INTEGER);
@@ -36,30 +36,32 @@ const marks = ref<{ [key: number]: string }>({});
 
 class MapItemListener extends taxonomy.ItemListener {
 
-  processed = false;
+  loaded = false;
   item: taxonomy.Item;
+  features: Map<number, GeoJSON.Feature> = new Map();
 
   constructor(item: taxonomy.Item) {
     super();
     this.item = item;
-    item.add_listener(this);
   }
 
   values(values: taxonomy.Data[]): void {
     for (const val of values)
       this.add_value(val);
-    this.process();
+    if (values.length > 0) {
+      this.loaded = true;
+      process_type(this.item.type);
+    }
   }
 
   new_value(value: taxonomy.Data): void {
     this.add_value(value);
-    this.process();
+    this.loaded = true;
+    process_type(this.item.type);
   }
 
   private add_value(value: taxonomy.Data): void {
-    if (!dynamic_features.has(value.timestamp.getTime())) {
-      dynamic_features.set(value.timestamp.getTime(), new Map());
-      dynamic_features.get(value.timestamp.getTime())!.set(this.item.type, { type: 'FeatureCollection', features: [] });
+    if (!marks.value[value.timestamp.getTime()]) {
       marks.value[value.timestamp.getTime()] = '';
       if (value.timestamp.getTime() < min.value)
         min.value = value.timestamp.getTime();
@@ -76,28 +78,7 @@ class MapItemListener extends taxonomy.ItemListener {
         properties[prop_name] = value.data[prop_name];
     for (const [prop_name, prop] of taxonomy.dynamic_properties(this.item.type))
       if (is_geometry(prop))
-        dynamic_features.get(value.timestamp.getTime())!.get(this.item.type)!.features.push({ type: 'Feature', geometry: value.data[prop_name], properties: properties });
-  }
-
-  private process(): void {
-    this.processed = true;
-    let proc = true;
-    for (const [_, itm_ls] of dynamic_items)
-      for (const [_, listener] of itm_ls)
-        if (!listener.processed) {
-          proc = false;
-          break;
-        }
-
-    if (proc) {
-      show_slider.value = true;
-      // Fill in missing layers with the previous one if not updated
-      const sorted = Array.from(dynamic_features.keys()).sort((a, b) => a - b);
-      for (let i = 1; i < sorted.length; i++)
-        for (const type of props.layers)
-          if (!dynamic_features.get(sorted[i])!.has(type) && dynamic_features.get(sorted[i - 1])!.has(type))
-            dynamic_features.get(sorted[i])!.set(type, dynamic_features.get(sorted[i - 1])!.get(type)!);
-    }
+        this.features.set(value.timestamp.getTime(), { type: 'Feature', geometry: value.data[prop_name], properties: properties });
   }
 }
 
@@ -162,12 +143,12 @@ watch(() => props.layers, (new_layers, old_layers) => {
         });
 
       if (dynamic_geometry) {
-        dynamic_items.set(l, new Map());
+        dynamic_items.set(l, { items: new Map(), loaded: false });
         coco.KnowledgeBase.getInstance().get_items(l).then(items => {
           console.debug('Dynamic geometry', l, items);
           for (const item of items) {
             const listener = new MapItemListener(item);
-            dynamic_items.get(l)!.set(item, listener);
+            dynamic_items.get(l)!.items.set(item, listener);
             item.add_listener(listener);
             if (item.values.length === 0)
               coco.KnowledgeBase.getInstance().load_data(item);
@@ -188,6 +169,70 @@ watch(() => time.value, (new_time, old_time) => {
     for (const [type, features] of dynamic_features.get(new_time)!)
       dynamic_layers.set(type, L.geoJSON(features, options.get(type)).addTo(map));
 });
+
+function process_type(type: taxonomy.Type): void {
+  let done = true;
+  for (const [_, listener] of dynamic_items.get(type)!.items)
+    if (!listener.loaded) {
+      done = false;
+      break;
+    }
+
+  if (done) {
+    console.debug('All data loaded for', type.name, 'type');
+
+    const c_features: Map<number, Map<taxonomy.Item, GeoJSON.Feature>> = new Map();
+    for (const [item, listener] of dynamic_items.get(type)!.items)
+      for (const [timestamp, feature] of listener.features) {
+        if (!c_features.has(timestamp))
+          c_features.set(timestamp, new Map());
+        c_features.get(timestamp)!.set(item, feature);
+      }
+
+    const sorted = Array.from(c_features.keys()).sort((a, b) => a - b);
+    for (let i = 1; i < sorted.length; i++)
+      for (const item of dynamic_items.get(type)!.items.keys())
+        if (!c_features.get(sorted[i])!.has(item) && c_features.get(sorted[i - 1])!.has(item))
+          c_features.get(sorted[i])!.set(item, c_features.get(sorted[i - 1])!.get(item)!);
+
+    for (const [timestamp, features] of c_features) {
+      if (!dynamic_features.has(timestamp))
+        dynamic_features.set(timestamp, new Map());
+      for (const [item, feature] of features) {
+        if (!dynamic_features.get(timestamp)!.has(item.type))
+          dynamic_features.get(timestamp)!.set(item.type, { type: 'FeatureCollection', features: [] });
+        dynamic_features.get(timestamp)!.get(item.type)!.features.push(feature);
+      }
+    }
+    dynamic_items.get(type)!.loaded = true;
+
+    process_types();
+  }
+}
+
+function process_types(): void {
+  let done = true;
+
+  for (const [_, itm_ls] of dynamic_items)
+    if (!itm_ls.loaded) {
+      done = false;
+      break;
+    }
+
+  if (done) {
+    console.debug('All data loaded');
+
+    // Fill in missing layers with the previous one if not updated
+    const sorted = Array.from(dynamic_features.keys()).sort((a, b) => a - b);
+    for (let i = 1; i < sorted.length; i++)
+      for (const type of props.layers)
+        if (!dynamic_features.get(sorted[i])!.has(type) && dynamic_features.get(sorted[i - 1])!.has(type))
+          dynamic_features.get(sorted[i])!.set(type, dynamic_features.get(sorted[i - 1])!.get(type)!);
+
+    show_slider.value = true;
+    time.value = sorted[sorted.length - 1];
+  }
+}
 </script>
 
 <script lang="ts">
