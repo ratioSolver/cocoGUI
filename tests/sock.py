64bc2375c2b1dbe41b3dc18d9bb08d0b48744eec
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2024-09-19 16:15:06 +0200

    Refactor WebSocket connection code in sock.py to allow custom URL configuration

--- tests/sock.py
+++ tests/sock.py
@@ -16,12 +16,8 @@ def on_open(ws):
     print("Opened connection")
 
 if __name__ == "__main__":
-    websocket.enableTrace(True)
-    ws = websocket.WebSocketApp("ws://localhost:8080",
-                              on_open=on_open,
-                              on_message=on_message,
-                              on_error=on_error,
-                              on_close=on_close)
+    url = sys.argv[1] if len(sys.argv) > 1 else 'ws://localhost:8080/coco'
+    ws = websocket.WebSocketApp(url, on_open=on_open, on_message=on_message, on_error=on_error, on_close=on_close)
 
     ws.run_forever(dispatcher=rel, reconnect=5)  # Set dispatcher to automatic reconnection, 5 second reconnect delay if connection closed unexpectedly
     rel.signal(2, rel.abort)  # Keyboard Interrupt
